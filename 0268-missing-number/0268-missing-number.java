e33ef34001bf02b0ca243239215e70b152cd0284
Kaviya Bharathi K <kaviyabharathi0805@gmail.com>
2026-08-22 10:42:36 +0530

    Time: 0 ms (100%), Space: 47.1 MB (82.59%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int SumOfn=(n*(n+1))/2;
+        int sumOfNums=0;
+        for(int i=0;i<n;i++){
+            sumOfNums=sumOfNums + nums[i];
+
+        }
+        int res=SumOfn-sumOfNums;
+        return res;
+
+
+    }
+}
